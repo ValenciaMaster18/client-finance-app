3c610b354dd67a2bbb6cc30f8be95e9b26c33c42
Luis valencia <luchito123410@gmail.com>
2023-04-17 09:54:52 -0500

    we link the sweetalert2 library

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['../../../../assets/css/auth/forms.scss']
 })
 export class LoginComponent {
-
 }
